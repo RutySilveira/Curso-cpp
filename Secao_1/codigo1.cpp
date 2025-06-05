411b038cb6af266ca71759085bba69854ad65f43
RutySilveira <ruteneles@gmail.com>
2025-06-05 15:21:21 -0300

    Adicionando exercícios

--- Secao_1/codigo1.cpp
+++ Secao_1/codigo1.cpp
@@ -1,16 +1,19 @@
 // Inclusão da biblioteca padrão de entrada e saída
 #include <iostream>
 
-// Função principal do programa - ponto de entrada
+// Função principal do programa - ponto de entrada, as chaves indicam o inicio de um bloco
 int main()
 {
   // Exibe o texto "Primeiro Codigo" no console seguido de uma quebra de linha
+  // cout comando de saída de dados
+  // neste caso está solicitando para colocar na saída padrão (tela) a frase especificada
+  // endl indica a inserção de um caractere de quebra de linha
   std::cout << "Primeiro Codigo" << std::endl;
 
   // Pausa a execução do programa até que o usuário pressione alguma tecla
-  // OBS: 'system("PAUSE")' é específico do Windows e não é recomendável em programas portáveis
+  // chama o shell do windows (DOS) e coloca o comando PAUSE
   system("PAUSE");
 
   // Retorna 0, indicando que o programa terminou corretamente
   return 0;
-}
+} // indica o final de um bloco
---
+++ Secao_2/exercicio_1.cpp
@@ -0,0 +1,14 @@
+// 01) Desenvolva um programa que escreve na tela a frase: Vou aprender a programar na
+// Linguagem C++
+
+#include <iostream>
+
+int main()
+{
+
+  std::cout << "Vou aprender a programar na Linguagem C++" << std::endl;
+
+  system("PAUSE");
+
+  return 0;
+}
